273e2ef244977cf80677dbd6f6f28ee3041121c8
Ermerson Rafael <ermersonrafael@hotmail.com>
2026-07-05 23:27:39 -0300

    Add RSS feed routes for pt and en

---
+++ app/pt/rss.xml/route.ts
@@ -0,0 +1,41 @@
+import { getPosts } from '../posts/get-posts'
+
+export const dynamic = 'force-static'
+
+const CONFIG = {
+  title: 'Blog Pessoal (PT)',
+  siteUrl: 'https://ermerson.github.io/pt',
+  description: 'Últimos posts do blog',
+  lang: 'pt-br'
+}
+
+export async function GET() {
+  const allPosts = await getPosts()
+  const items = allPosts
+    .map(
+      post => `    <item>
+        <title>${post.frontMatter.title}</title>
+        <description>${post.frontMatter.description ?? ''}</description>
+        <link>${CONFIG.siteUrl}${post.route}/</link>
+        <pubDate>${new Date(post.frontMatter.date).toUTCString()}</pubDate>
+    </item>`
+    )
+    .join('\n')
+
+  const xml = `<?xml version="1.0" encoding="UTF-8" ?>
+<rss version="2.0">
+  <channel>
+    <title>${CONFIG.title}</title>
+    <link>${CONFIG.siteUrl}</link>
+    <description>${CONFIG.description}</description>
+    <language>${CONFIG.lang}</language>
+${items}
+  </channel>
+</rss>`
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/rss+xml'
+    }
+  })
+}
