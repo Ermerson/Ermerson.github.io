1b56c6c6ea0ac27aff0c0f0a8d8651681e0ae025
Ermerson Rafael <ermersonrafael@hotmail.com>
2026-07-05 23:15:42 -0300

    Wire up Nextra blog theme, MDX pipeline, and first bilingual posts

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from '@/components/ui/button'
-
-export default function Home() {
-  return (
-    <div style={{ padding: 40 }}>
-      <Button data-testid="shadcn-smoke-test">Hello shadcn</Button>
-    </div>
-  )
-}
